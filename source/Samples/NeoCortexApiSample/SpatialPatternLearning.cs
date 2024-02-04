b73089d07d25698cbaaa1f8a5568e81e3329665a
Md Imon Bhuiya <imonbhuiya2@gmail.com>
2024-02-04 23:48:55 +0100

    Print Thresholded Probability

--- source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
+++ source/Samples/NeoCortexApiSample/SpatialPatternLearning.cs
@@ -230,6 +230,8 @@ namespace NeoCortexApiSample
                 var probabilityValues = probabilities.Values;
                 var ThresholdValue = 8.3;
                 string ThresholdedPermanence = Helpers.ConvertValuesToBinaryString(probabilityValues, ThresholdValue);
+                Debug.WriteLine($"Thresholded Probability: {ThresholdedPermanence}");
+
 
 
             }
